eb0b442c14a5f6d4ca4c2054559f9a85cee6599e
H. Turgut Uyar <uyar@tekir.org>
2018-12-26 19:51:42 +0300

    docs: Add code documentation

--- gulliver/__init__.py
+++ gulliver/__init__.py
@@ -1,9 +1,17 @@
+"""Initialization of the app."""
+
 __version__ = "0.1.0"
 
 from pyramid.config import Configurator
 
 
 def create_app(global_config, **settings):
+    """Create a Gulliver app.
+
+    :param global_config: Global configuration settings.
+    :param settings: Extra settings to pass to the configurator.
+    :return: Created application.
+    """
     config = Configurator(settings=settings)
     config.include("pyramid_chameleon")
     config.scan(".views")
--- gulliver/views.py
+++ gulliver/views.py
@@ -1,7 +1,14 @@
+"""Handlers for the HTTP requests to the endpoints of the app."""
+
 from pyramid.view import view_config
 
 
 @view_config(name="", renderer="templates/home.pt")
 def home(request):
+    """Respond to a request to the root.
+
+    :param request: Incoming HTTP request.
+    :return: Page data.
+    """
     page_title = "Gulliver"
     return {"page_title": page_title}
