dac7c7ca495064ec206efdde15c5753ed720feb0
H. Turgut Uyar <uyar@tekir.org>
2018-12-26 19:26:14 +0300

    feat: Add initial version of app

--- gulliver/__init__.py
+++ gulliver/__init__.py
@@ -1,1 +1,10 @@
 __version__ = "0.1.0"
+
+from pyramid.config import Configurator
+
+
+def create_app(global_config, **settings):
+    config = Configurator(settings=settings)
+    config.include("pyramid_chameleon")
+    config.scan(".views")
+    return config.make_wsgi_app()
---
+++ gulliver/views.py
@@ -0,0 +1,7 @@
+from pyramid.view import view_config
+
+
+@view_config(name="", renderer="templates/home.pt")
+def home(request):
+    page_title = "Gulliver"
+    return dict(page_title=page_title)
