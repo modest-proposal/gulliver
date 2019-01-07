b950c287fe70b57668cd323108dd1ae3b8da3ade
H. Turgut Uyar <uyar@tekir.org>
2019-01-07 18:26:42 +0300

    tests: Add ZODB URI to testing setup

--- tests/test_gulliver_functional.py
+++ tests/test_gulliver_functional.py
@@ -1,15 +1,23 @@
 import webtest
 from pytest import fixture
 
+import os
+from configparser import ConfigParser
 from http.server import HTTPStatus
 
 from gulliver import create_app
 
 
+_CONFIG_FILE = os.path.join(os.path.dirname(__file__), "testing.ini")
+
+
 @fixture(scope="module")
 def app():
     """A Gulliver application instance for testing."""
-    app_ = create_app({})
+    parser = ConfigParser()
+    parser.read(_CONFIG_FILE)
+    settings = parser["app:main"]
+    app_ = create_app({}, **settings)
     return webtest.TestApp(app_)
 
 
