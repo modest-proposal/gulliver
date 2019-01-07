46df51989ea903a37aecf727adf50243afb73452
H. Turgut Uyar <uyar@tekir.org>
2019-01-07 17:04:46 +0300

    setup: Add copyright notices

--- gulliver/__init__.py
+++ gulliver/__init__.py
@@ -1,4 +1,21 @@
+# Copyright (C) 2018-2019 H. Turgut Uyar <uyar@tekir.org>
+#
+# Gulliver is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Gulliver is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Gulliver.  If not, see <http://www.gnu.org/licenses/>.
+
+
 """Initialization of the app."""
+
 
 __version__ = "0.1.0"  # sig: str
 
--- gulliver/views.py
+++ gulliver/views.py
@@ -1,3 +1,19 @@
+# Copyright (C) 2018-2019 H. Turgut Uyar <uyar@tekir.org>
+#
+# Gulliver is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Gulliver is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Gulliver.  If not, see <http://www.gnu.org/licenses/>.
+
+
 """Handlers for the HTTP requests to the endpoints of the app."""
 
 from pyramid.view import view_config
