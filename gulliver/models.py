2b6a3d767e0f641229e913bb71d89b7dec998193
H. Turgut Uyar <uyar@tekir.org>
2019-01-07 20:26:06 +0300

    docs: Add license file

--- gulliver/models.py
+++ gulliver/models.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2018-2019 H. Turgut Uyar <uyar@tekir.org>
+# Copyright (C) 2019 H. Turgut Uyar <uyar@tekir.org>
 #
 # Gulliver is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
